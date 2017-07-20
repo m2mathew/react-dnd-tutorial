51bff18f0f76d931835a49ae31e39cd651ddadbb
Mike Mathew <m2mathew@me.com>
2017-07-19 23:32:15 -0500

    Partial implementationn of Game functions

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,21 @@
-export function observe(receive) {
-  setInterval(() => receive([
-    Math.floor(Math.random() * 8),
-    Math.floor(Math.random() * 8)
-  ]), 900);
+let knightPosition = [0, 0];
+let observer = null;
+
+function emitChange() {
+  observe(knightPosition);
+}
+
+
+export function observe(o) {
+  if (observer) {
+    throw new Error('Multiple observers not implemented.');
+  }
+
+  observer = o;
+  emitChange();
+}
+
+export function moveKnight(toX, toY) {
+  knightPosition = [toX, toY];
+  emitChange();
 }
